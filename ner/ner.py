adcd48f084922c192f574d11fcfb35127d13d14e
Ali Sobhani <al.sbhn@gmail.com>
2020-12-09 21:25:07 +0000

    ner

--- ner/ner.py
+++ ner/ner.py
@@ -3,6 +3,7 @@ import json
 from sklearn.utils import shuffle
 from transformers import BertTokenizer
 from tokenizer import split_train_test, tokenize_dataset
+from torch.utils.data import TensorDataset, random_split
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
@@ -19,4 +20,22 @@ labels = train_set.ner.values
 
 input_ids, attention_masks, labels = tokenize_dataset(sentences_1 , sentences_2, labels, 512, tokenizer)
 
-print (len(input_ids))
+# Divide up our training set to use 90% for training and 10% for validation.
+dataset = TensorDataset(input_ids, attention_masks, labels)
+train_size = int(0.9 * len(dataset))
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+print('{:>5,} training samples'.format(train_size))
+print('{:>5,} validation samples'.format(val_size))
+
+# Prepare for trainig
+train_dataloader, validation_dataloader = build_data_loader(batch_size = 8)
+model = build_model(num_labels = 2)
+optimizer = build_optimizer(lr = 2e-5,eps = 1e-8)
+scheduler = build_scheduler (train_dataloader, optimizer , epochs = 15)
+
+
+
+
+
+
--- ner/train.py
+++ ner/train.py
@@ -1,4 +1,4 @@
-from torch.utils.data import TensorDataset, random_split
+
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 
 from transformers import BertForSequenceClassification, AdamW, BertConfig
@@ -24,8 +24,25 @@ def build_optimizer(lr = 2e-5,eps = 1e-8):
   optimizer = AdamW(model.parameters(), lr = lr, eps = eps)
   return optimizer
 
-def build_scheduler (train_dataloader, optimizer , epochs = 15)
+def build_scheduler (train_dataloader, optimizer , epochs = 15):
   total_steps = len(train_dataloader) * epochs
   scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0, num_training_steps = total_steps)
   return scheduler
 
+# Define a helper function for calculating accuracy.
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+# Helper function for formatting elapsed times as hh:mm:ss
+def format_time(elapsed):
+    '''
+    Takes a time in seconds and returns a string hh:mm:ss
+    '''
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+    
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
